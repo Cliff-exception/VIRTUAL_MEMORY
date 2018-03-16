1079694602ba3f9a2b8b161ec0fa400f9695b918
Tyson E. Morris <tem97@utility.cs.rutgers.edu>
2018-03-16 18:29:02 -0400

    Finished preparations to start memory mapping via virtual memory.

--- myalloc.c
+++ myalloc.c
@@ -1,63 +1,39 @@
 #include "myalloc.h"
 void pages_init(){ //chunk mem into pages of 4kb
 
-     int curr_page = KERNEL_MEMORY;
+    int curr_page = KERNEL_MEMORY;
+    size_t block_size = PAGE_SIZE - 2*sizeof(block_meta);
 
-	 while(curr_page < page_num){
-	 
-	 /*
-	 	page_meta temp_page;
-	 	
-	 	temp_page.start = &mem_block[curr_page * PAGE_SIZE];
-	 	temp_page.start_idx = curr_page * PAGE_SIZE; // debug purpose
-	 	temp_page.tid = -1;
-	 	temp_page.page_id = curr_page; 
-	 	temp_page.free_size = PAGE_SIZE;
-	 	
-	 	//printf("pagenum:%d page addr%d",i,curr_page * PAGE_SIZE);
-	 	page_meta * temp = (page_meta*)&mem_block[i *sizeof(page_meta)];
-	 	memcpy(temp,&temp_page,sizeof(page_meta));
-	 
-	 	block_meta temp_block;
-	 	//fill the block
-	 		temp_block.istaken = 0;
-	 		temp_block.blk_size = PAGE_SIZE - 2*sizeof(block_meta);
-	 		temp_block.prev = NULL;
-			temp_block.next = NULL;
-	 	//
-	 	
-	 	//printf("init block:\n");
-	 	//print_blk_meta(&temp_block);
-	 	
-	 	memcpy(temp->start,&temp_block,sizeof(block_meta));// copy to lower bound
-	 	temp->blk_list = (block_meta*)temp->start;
-	 	
-	 	*/
-	 	block_meta temp_block;
-	 	//fill the block
-	 		temp_block.istaken = 0;
-	 		temp_block.blk_size = PAGE_SIZE - 2*sizeof(block_meta);
-	 		temp_block.prev = NULL;
-			temp_block.next = NULL;
-	 	//
-	 	
-	 	//printf("init block:\n");
-	 	//print_blk_meta(&temp_block);
-	 	
-	 	memcpy(&mem_block[curr_page * PAGE_SIZE],&temp_block,sizeof(block_meta));// copy to lower bound
-	 	
-	 	
-	 	memcpy(&mem_block[(curr_page+1) * PAGE_SIZE - sizeof(block_meta)],&temp_block,sizeof(block_meta)); //copy to upper bound
-	 	
-	 		//printf("lower in a page: %d\nhiger in a page: %d\n",curr_page * PAGE_SIZE,(curr_page+1) * PAGE_SIZE - sizeof(block_meta));
-	 		//just some code might not even work
-	 	//printf("\tdata addr%d\n",i,i*sizeof(page_meta)+sizeof(block_meta));
-	 	
-	 	curr_page++;
-	 	
-	 }
+    while(curr_page < page_num){
+     
+        block_meta temp_block;
+        temp_block.p_type = UNASSIGNED;
+        temp_block.blk_size = block_size;
+        temp_block.free_size = block_size;
+        temp_block.tid = -1;
+        temp_block.prev = NULL;
+        temp_block.next = NULL;
+        
+        // copy to lower bound
+        memcpy(&mem_block[curr_page * PAGE_SIZE],
+               &temp_block,
+               sizeof(block_meta));
+        
+        //copy to upper bound
+        memcpy(&mem_block[(curr_page+1) * PAGE_SIZE - sizeof(block_meta)],
+               &temp_block,
+               sizeof(block_meta)); 
+        
+        curr_page++;
+    }
+
+    // Ensure that the first user block is of type THREAD, it will be used for
+    // dereferencing memory addresses.
+    block_meta * temp_block = (block_meta*) &mem_block[MAIN_PAGE];
+    temp_block->p_type = THREAD;
 }
 
+/* Tyson - Commenting out for time being, need to fix to work with memory mapping.
 page_meta * get_page(int page_id){
     assert(page_id>=KERNEL_MEMORY);
     return (page_meta*)&mem_block[(page_id-KERNEL_MEMORY) * sizeof(page_meta)];
@@ -74,19 +50,53 @@ void print_page_meta(page_meta * page){
     
     printf("free size:%d\ntid: %d\n\n\n\n\n",page->free_size,page->tid);
 
-}
+} */
+
 void print_blk_meta(block_meta * blk){
 
     if(!blk) return;
     printf("size lower: %d\n",blk->blk_size);
     
-    printf("size upper: %d\n",((block_meta *)(((long)blk)+blk->blk_size+sizeof(block_meta)))->blk_size);
+    printf("size upper: %d\n",
+           ((block_meta *)(((long)blk) + 
+                           blk->blk_size +
+                           sizeof(block_meta)))->blk_size);
     
     printf("next block:\n");
     print_blk_meta(blk->next);
 
 }
-block_meta * find_block(block_meta * blk_list, size_t x){ //given size x, find the first fit block in a list of blocks
+
+block_meta * find_block(int tid_req, size_t x) {
+    // If x is larger than size of memory in a page, at this point we can't
+    // hold it and must reject.
+    if (x > (PAGE_SIZE - 2 * sizeof(block_meta)))
+        return NULL;
+
+    block_meta * temp  = NULL;  // Holds current page under inspection.
+    block_meta * first = NULL;  // First UNASSIGNED page found just in case.
+    
+    // Look for a page that is used by the given tid_req.
+    int i = 0;
+    for ( ; i < USER_PAGES; i++) {
+        temp = (block_meta*) &mem_block[(MAIN_PAGE + PAGE_SIZE) + 
+                                        (i * PAGE_SIZE)];
+        if (first == NULL && temp->p_type == UNASSIGNED)
+            first = temp;
+        else if (temp->p_type != UNASSIGNED && temp->tid == tid_req) {
+            block_meta * enough_room = find_block_in_page(temp, x);
+            if (enough_room != NULL)
+                return enough_room;
+        }
+    }
+
+    // At this point, all pages with given tid = tid_req don't have enough
+    // room for storing x bits. Using the first found UNASSIGNED page.
+    return first;
+}
+
+// given size x, find the first fit block in a list of blocks
+block_meta * find_block_in_page(block_meta * blk_list, size_t x){ 
     block_meta * temp = blk_list;
 
     int i =0;
@@ -94,7 +104,7 @@ block_meta * find_block(block_meta * blk_list, size_t x){ //given size x, find t
     
         //printf("block num:%d   size: %d\n",i++,temp->blk_size);
         
-        if(!temp->istaken &&
+        if(temp->p_type == UNASSIGNED &&
             (temp->blk_size >= ATTEMP_TAKE)){
             return temp;
         }
@@ -102,8 +112,8 @@ block_meta * find_block(block_meta * blk_list, size_t x){ //given size x, find t
     }
     
     return NULL;
-
 }
+
 //void * myallocate(size_t x, __FILE__, __LINE__, THREADREQ){
 void * myallocate(size_t x, char * file, int linenum, int tid_req){
     
@@ -117,13 +127,10 @@ void * myallocate(size_t x, char * file, int linenum, int tid_req){
     curr_page->tid = tid_req;
     */
     
-    //TODO: find a page by given tid_req, and assign it to curr_page
-    void * curr_page;
-    
     //printf("page:%d\n",curr_page->page_id);
     //block_meta * first_fit = find_block(curr_page->blk_list,x);
     
-    block_meta * first_fit = find_block(curr_page,x);
+    block_meta * first_fit = find_block(tid_req, x);
     if(!first_fit) {
         printf("page full!\n");
     
@@ -133,27 +140,32 @@ void * myallocate(size_t x, char * file, int linenum, int tid_req){
     printf("before malloc: \n");
     print_blk_meta(first_fit);
 */  
-    first_fit -> istaken = 1;
+    first_fit->p_type = DATA;
     block_meta * temp_addr = first_fit->next;
-    block_meta * old_upper = (block_meta *)((long)(first_fit + 1) + (first_fit->blk_size));
+    block_meta * old_upper = (block_meta *)((long)(first_fit + 1) + 
+                                            (first_fit->blk_size));
     size_t old_size = first_fit -> blk_size;
     
-    
-    first_fit -> next = (block_meta*)((long)first_fit + ATTEMP_TAKE);//(x + 2*sizeof(block_meta)); and points to the compliment of taken block
+    //(x + 2*sizeof(block_meta)); and points to the compliment of taken block
+    first_fit -> next = (block_meta*)((long)first_fit + ATTEMP_TAKE);
     
     // fill a new block, the compliment of taken block
     block_meta new_block;
-    new_block.istaken = 0;
+    new_block.p_type = UNASSIGNED;
     new_block.blk_size = old_size - ATTEMP_TAKE;
     new_block.next = temp_addr;
-    memcpy(first_fit->next, &new_block, sizeof(block_meta)); //set the lower bound for a free block
+
+    //set the lower bound for a free block
+    memcpy(first_fit->next, &new_block, sizeof(block_meta)); 
     
-    old_upper->blk_size = new_block.blk_size;       //set the upper bound for a free block
+    //set the upper bound for a free block
+    old_upper->blk_size = new_block.blk_size;       
     
+    // set the boundary tag for lower addr 
+    first_fit -> blk_size = x;            
     
-    first_fit -> blk_size = x;              // set the boundary tag for lower addr
-    
-    memcpy( (void*)(  ((long)(first_fit+1)) + x ) ,first_fit,sizeof(block_meta));//set the boundray tag for higher addr
+    //set the boundray tag for higher addr
+    memcpy( (void*) (( (long)(first_fit+1) ) + x), first_fit,sizeof(block_meta));
     // ugly & very subject to bug
 /*
     printf("after malloc: \n");
@@ -176,16 +188,18 @@ void mydeallocate(void * ptr, char * file, int linenum, int tid_req){
     print_blk_meta(lower_bound);
     */
     
-    lower_bound->istaken = 0;
-    upper_bound->istaken = 0;
+    lower_bound->p_type = UNASSIGNED;
+    upper_bound->p_type = UNASSIGNED;
     
     size_t new_size;
-    if((upper_bound+1)->istaken == 0 && 
+    if((upper_bound+1)->p_type == UNASSIGNED && 
         (((long)(upper_bound+1) - (long)&mem_block)% PAGE_SIZE != 0 )){ //coalescing with a higher free block
         
         //printf("coalescing with a higher free block\n");
         
-        new_size = lower_bound -> blk_size + (upper_bound+1)->blk_size + 2*sizeof(block_meta);
+        new_size = lower_bound -> blk_size + 
+                                  (upper_bound+1)->blk_size + 
+                                  2*sizeof(block_meta);
         block_meta * new_upper = (void *) ((long)(lower_bound + 1) + new_size);
         
         lower_bound -> blk_size = new_size;
@@ -200,7 +214,7 @@ void mydeallocate(void * ptr, char * file, int linenum, int tid_req){
         */
     }
     
-    if((lower_bound-1)->istaken == 0 &&     
+    if((lower_bound-1)->p_type == UNASSIGNED &&     
             ((((long)lower_bound) - (long)&mem_block)% PAGE_SIZE != 0 )){   //coalescing with a lower free block
     
         //(((long)lower_bound)% PAGE_SIZE != 0 )){ 
@@ -209,8 +223,11 @@ void mydeallocate(void * ptr, char * file, int linenum, int tid_req){
     
         //printf("coalescing with a lower free block\n");
         
-        new_size =  lower_bound -> blk_size + (lower_bound-1)->blk_size + 2*sizeof(block_meta);
-        block_meta * new_lower = (void *) ((long)(lower_bound - 1) - (lower_bound-1)->blk_size);
+        new_size =  lower_bound->blk_size + 
+                    (lower_bound-1)->blk_size + 
+                    2*sizeof(block_meta);
+        block_meta * new_lower = (void *) ((long)(lower_bound - 1) - 
+                                           (lower_bound-1)->blk_size);
         
         //printf("new size %d = %d + %d + 2*meta:%d\n",new_size, lower_bound -> blk_size ,(lower_bound-1)->blk_size,2*sizeof(block_meta));
         upper_bound -> blk_size = new_size;
@@ -253,8 +270,6 @@ int get_offset(unsigned long memory_address) {
 }
 
 int main(){
-    /*
-    
     //num_page = MEM_SIZE/ (sizeof(page_meta)+ PAGE_SIZE) ; AND TAKE THE FLOOR
     pages_init();
     char * test0 = (char *) myallocate(15,__FILE__,__LINE__,2);
--- myalloc.h
+++ myalloc.h
@@ -13,6 +13,7 @@
 #define MEM_SIZE (8*1024*1024)
 #define PAGE_SIZE (4*1024)
 #define KERNEL_MEMORY 32
+#define MAIN_PAGE (KERNEL_MEMORY * PAGE_SIZE)
 #define LIBRARYREQ -1
 //#define THREADREQ 1
 #define SHARED_PAGE 4
@@ -20,7 +21,11 @@
 #define ATTEMP_TAKE (x+2*sizeof(block_meta))
 static char mem_block[MEM_SIZE];
 static int page_num = MEM_SIZE/PAGE_SIZE; //should be 2048 without tune it
+static int USER_PAGES = (MEM_SIZE - (KERNEL_MEMORY * PAGE_SIZE)) / PAGE_SIZE;
 
+typedef enum Page_Type {UNASSIGNED, THREAD, DATA} page_type;
+
+/* Tyson - Commented out ken's structs in case changes fail.
 typedef struct blk{
     int istaken;
     size_t blk_size;
@@ -39,14 +44,25 @@ typedef struct _PageData{
     struct blk * blk_list;//implicit free list
     //struct _PageData *next;
 }page_meta;
+*/
 
+typedef struct blk{
+    page_type p_type;
+    size_t blk_size;
+    size_t free_size;
+    int tid;
+
+    struct blk * prev;
+    struct blk * next;
+}block_meta;
 
 void pages_init();
-page_meta * get_page(int page_id);
-page_meta * find_page(int tid);
-void print_page_meta(page_meta * page);
+//page_meta * get_page(int page_id);
+//page_meta * find_page(int tid);
+//void print_page_meta(page_meta * page);
 void print_blk_meta(block_meta * blk);
-block_meta * find_block(block_meta * blk_list, size_t x);
+block_meta * find_block(int tid_req, size_t x);
+block_meta * find_block_in_page(block_meta * blk_list, size_t x);
 void * myallocate(size_t x, char * file, int linenum, int tid_req);
 void mydeallocate(void * ptr, char * file, int linenum, int tid_req);
 
